926310db14bba98b0346346b3619ca8e171ee647
Shubham Gupta <shubham272101@gmail.com>
2025-05-28 20:57:36 +0530

    add CPP folder and syntax file

---
+++ CPP/syntax.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    cout<<"hello world "<<endl;
+    cout<<"Value of num is : "<<num<<endl;
+    return 0;
+}
+
+
+// scanf("%d",&num)
+// printf("heelo work %d",num)
+
+// cpp ch input lyi  = c + in = cin >>
+// cpp ch output lyi  = c + out = cout <<
+//   
